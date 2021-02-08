03ec5bb955464644af23fa3c96408170662397ff
raindroid <yucan.wu@outlook.com>
2021-02-07 23:45:34 -0500

    Delete secret.js

--- frontend/src/components/account/secret.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAygxpgWknOZuFc9dNpVQ6P8jdeBiD-hIM",
-    authDomain: "maximal-relic-304001.firebaseapp.com",
-    projectId: "maximal-relic-304001",
-    storageBucket: "maximal-relic-304001.appspot.com",
-    messagingSenderId: "406361332644",
-    appId: "1:406361332644:web:8b7887bf8c640a1b797f31",
-    measurementId: "G-JKDJ2NGRPK"
-  };
